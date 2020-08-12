57520842251764f82df9cc28c4ba82f6ac004595
Felipe Felix <felipfelixsantos07@gmail.com>
2020-08-12 12:41:24 -0300

    Finalização da página give-classes com javascript

---
+++ scripts/addField.js
@@ -0,0 +1,22 @@
+//Procurando o botão
+
+document.querySelector('#add-time')
+
+.addEventListener('click', cloneField)
+
+function cloneField() {
+    //duplicar campos
+        const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+    
+    //Pegar os campos para depois limpar
+        const fields = newFieldContainer.querySelectorAll('input');
+
+    //Limpar cada campo
+    fields.forEach(function (field) {
+    //Pegar os fields
+        field.value = "";
+    });
+
+    //colocar na página
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
