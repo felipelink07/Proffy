d9da70940dee80220b501226a5941cda959075f5
Felipe Felix <felipfelixsantos07@gmail.com>
2020-08-15 10:25:12 -0300

    Mudanças no addField.js

--- scripts/addField.js
+++ scripts/addField.js
@@ -2,19 +2,20 @@
 
 document.querySelector('#add-time')
 
-.addEventListener('click', cloneField)
+    .addEventListener('click', cloneField)
 
 function cloneField() {
     //duplicar campos
-        const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
-    
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
     //Pegar os campos para depois limpar
-        const fields = newFieldContainer.querySelectorAll('input');
+    const fields = newFieldContainer.querySelectorAll('input');
 
     //Limpar cada campo
     fields.forEach(function (field) {
-    //Pegar os fields
+        //Pegar os fields
         field.value = "";
+        []
     });
 
     //colocar na página
